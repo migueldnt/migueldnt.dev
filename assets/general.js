b06c6d92f72a4b0c24f28a5f53dc092c876b1a75
Miguel <miguel.danto@gmail.com>
2020-12-25 10:17:38 -0600

    adecuaciones en el nav

--- assets/general.js
+++ assets/general.js
@@ -1,1 +1,10 @@
-console.log("hola mundo");
+document.addEventListener('DOMContentLoaded', () => {
+    let toggler=Array.prototype.slice.call(document.querySelectorAll('.navbar-toggler'), 0);
+    //console.log(toggler[0])
+
+    let target=document.getElementById( toggler[0].dataset.target );
+    toggler[0].addEventListener("click", ()=>{
+        toggler[0].classList.toggle("is-active")
+        target.classList.toggle("show")
+    })
+});
